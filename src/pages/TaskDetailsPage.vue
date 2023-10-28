cacb442c4a228c8b2ad09c9ea8328a91f4b68f02
Joseph Harwood <josephblakeharwood@gmail.com>
2023-10-27 23:31:13 -0400

    Revert "added cursor pointer to buttons"
    This reverts commit 0082f53508925b16e3e6ed955a0d0bfdeaf28a38.

--- src/pages/TaskDetailsPage.vue
+++ src/pages/TaskDetailsPage.vue
@@ -1,28 +1,14 @@
 <template>
-    <q-item v-ripple v-if="task" style="max-width: 318px">
-        <q-item-section>
-            <q-item-label>
-                {{ task.title }}
-            </q-item-label>
-            <q-item-label caption>
-                {{ task.id }}
-            </q-item-label>
-            <q-item-label caption>
-                {{ task.isFav }}
-            </q-item-label>
-        </q-item-section>
-        <q-item-section side top>
-            <q-item-label caption>
-                <button @click="store.deleteTasks(task.id)">delete</button>
-            </q-item-label>
-            <q-item-label caption>
-                <button @click="store.toggleFav(task.id)">toggle fav</button>
-            </q-item-label>
-        </q-item-section>
-    </q-item>
-    <q-item-label caption>
+    <div v-if="task">
+        <div>{{ task.id }}</div>
+        <div>{{ task.title }}</div>
+        <div>{{ task.isFav }}</div>
+        <router-link to="/">
+            <button @click="store.deleteTasks(task.id)">delete</button>
+        </router-link>
+        <button @click="store.toggleFav(task.id)">toggle fav</button>
         <button @click="$router.go(-1)">back</button>
-    </q-item-label>
+    </div>
 </template>
 
 <script setup lang="ts">
